03a1959868fbdf966ccebb7de16fac9a7296a0f1
jdm79 <james.malvern@gmail.com>
2018-09-20 20:39:59 +0100

    adds preventDefault to the submit eventlistener

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -4,11 +4,23 @@
     this._noteList = list
     this._noteListView = view
   }
+  NoteController.prototype.setEventListeners = function(){
+    document.getElementById('submit')
+      .addEventListener('submit', this.submit.bind(this));
+  }
+
+  NoteController.prototype.submit = function(){
+    let noteInput = document.getElementById('text').value;
+    this.displayResult(noteInput);
+    event.preventDefault();
+  }
 
   NoteController.prototype.listItems = function() {
     var listItems = document.getElementById('list-items')    
     listItems.innerHTML = this._noteListView.htmlize(this._noteList.show())
   }
+
+
 
   exports.NoteController = NoteController
 })(this)
