dcd04b3f430613ef138be45dddec0a660c7111f5
jdm79 <james.malvern@gmail.com>
2018-09-20 19:08:48 +0100

    puts files in appropriate folders

---
+++ controllers/note-controller.js
@@ -0,0 +1,14 @@
+(function(exports){
+
+  function NoteController(list, view = new NoteListView()) {
+    this._noteList = list
+    this._noteListView = view
+  }
+
+  NoteController.prototype.listItems = function() {
+    var listItems = document.getElementById('list-items')    
+    listItems.innerHTML = this._noteListView.htmlize(this._noteList.show())
+  }
+
+  exports.NoteController = NoteController
+})(this)
