418da1413d6b80d4bacccea3a14064f61addf56c
jdm79 <james.malvern@gmail.com>
2018-09-20 22:28:36 +0100

    adds node_modules to .gitignore, puts all the js and css code in a src file

--- src/controllers/note-controller.js
+++ src/controllers/note-controller.js
@@ -4,23 +4,21 @@
     this._noteList = list
     this._noteListView = view
   }
-  NoteController.prototype.setEventListeners = function(event){
+  NoteController.prototype.setEventListeners = function(){
     window.addEventListener('submit', this.log.bind(this));
     // event.preventDefault();
-
   }
 
   NoteController.prototype.log = function(event){
-    console.log("this button has been clicked")
-    // event.preventDefault();
+    console.log(event)
+    document.getElementById('demo').innerHTML = "yo"
+    event.preventDefault();
   }
 
   NoteController.prototype.listItems = function() {
     var listItems = document.getElementById('list-items')    
     listItems.innerHTML = this._noteListView.htmlize(this._noteList.show())
   }
-
-
 
   exports.NoteController = NoteController
 })(this)
