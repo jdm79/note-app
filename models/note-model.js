dfa1a98e64fdd37eaf3a6084a4e175515c73abd3
jdm79 <james.malvern@gmail.com>
2018-09-20 19:19:05 +0100

    adds test for note text, adds the method for returning note text

--- models/note-model.js
+++ models/note-model.js
@@ -1,10 +1,18 @@
 (function(exports) {
 
   var idcounter = 1
-  
+
   function Note(text) {
     this.text = text
     this.id = idcounter++
+  }
+
+  Note.prototype.getNoteText = function() {
+    return this.text
+  }
+
+  Note.prototype.getNoteTextId = function() {
+    return this.id
   }
 
   exports.Note = Note;
--- tests/note-model-test.js
+++ tests/note-model-test.js
@@ -9,15 +9,15 @@
   }
   testNoteText();
 
-  // function testNoteTextMethod() {
-  //   var note = new Note("hello");
-  //   console.log("this is our note id: " + note.id)
-  //   if (note.getNoteText() !== "hello") {
-  //     throw new Error("Text does not match")
-  //   }
-  //   else console.log("probably worked")
-  // }
-  // testNoteTextMethod();
+  function testNoteTextMethod() {
+    var note = new Note("hello");
+    console.log("this is our note id: " + note.id)
+    if (note.getNoteText() !== "hello") {
+      throw new Error("Text does not match")
+    }
+    else console.log("probably worked")
+  }
+  testNoteTextMethod();
 
   // function testNoteTextId() {
   //   var note = new Note("hello")
